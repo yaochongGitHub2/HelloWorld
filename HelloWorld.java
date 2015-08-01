682676c21679d3209f6ceb2ebc930f57857f18ff
yaochongGitHub2 <825296909@qq.com>
2015-08-01 15:54:45 +0800

    update this file
    add "hello GitHub" String to print

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ class
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
+                System.out.println("Hello GitHub");
 	}
 }
 //https://github.com/yaochongGitHub/HelloWorld.git
